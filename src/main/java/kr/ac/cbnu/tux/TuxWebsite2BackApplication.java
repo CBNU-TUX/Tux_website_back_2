d3c86be7f9e900aa8ea805311c698985853461b4
Woochang Lee <changi112242@outlook.com>
2023-07-23 15:59:38 +0900

    Init spring boot project

---
+++ src/main/java/kr/ac/cbnu/tux/TuxWebsite2BackApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.cbnu.tux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuxWebsite2BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuxWebsite2BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/cbnu/tux/TuxWebsite2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.cbnu.tux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuxWebsite2BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
